0d877f6ed5bb36439896c5069ed59223c7bca8be
andresr31 <carlos.andres.1031@gmail.com>
2020-04-28 15:17:40 -0500

    First commit

---
+++ src/mensajeriarmi/georreferenciacion/Conexion.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mensajeriarmi.georreferenciacion;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Conexion con la BD.
+ * 
+ * @author Carlos Andres Rojas
+ * @author David Salgado Ospina
+ * 
+ * 
+ */
+public class Conexion {
+    
+    private Connection conn;
+    
+    public Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:georreferenciaColombia.db";
+        conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println("Conexión DB exitosa");
+        } catch (SQLException e) {
+            System.out.println("Falla en conexión DB: " + e.getMessage());
+        }
+        return conn;
+    }
+     
+    public void closeConnection(Connection conn){
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println("No se pudo cerrar la conexión");
+        }
+    }
+}
---
+++ src/mensajeriarmi/paquete/Paquete.java
@@ -0,0 +1,142 @@
+
+package mensajeriarmi.paquete;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Carlos Andres Rojas Parra
+ * @author David Salgado Ospina
+ * 
+ */
+public class Paquete {
+    
+    private String nombreEmisor;
+    private String ciudadEmisor;
+    private String nombreReceptor;
+    private String ciudadReceptor;
+    private String departamentoReceptor;
+    private double latitudReceptor;
+    private double longitudReceptor;
+    private Date fechaRecibido;
+    private Date fechaEnvio;
+    private Date horaRecibido;
+    private Date horaEnvio;
+    private String estado; // RECIBIDO, ENVIADO, EN PROCESO
+    private double peso;
+    
+    ///////////////////////////////////////////////////////////////////////////
+
+    public Paquete() {
+        
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+
+    public String getNombreEmisor() {
+        return nombreEmisor;
+    }
+
+    public void setNombreEmisor(String nombreEmisor) {
+        this.nombreEmisor = nombreEmisor;
+    }
+
+    public String getCiudadEmisor() {
+        return ciudadEmisor;
+    }
+
+    public void setCiudadEmisor(String ciudadEmisor) {
+        this.ciudadEmisor = ciudadEmisor;
+    }
+
+    public String getNombreReceptor() {
+        return nombreReceptor;
+    }
+
+    public void setNombreReceptor(String nombreReceptor) {
+        this.nombreReceptor = nombreReceptor;
+    }
+
+    public String getCiudadReceptor() {
+        return ciudadReceptor;
+    }
+
+    public void setCiudadReceptor(String ciudadReceptor) {
+        this.ciudadReceptor = ciudadReceptor;
+    }
+
+    public String getDepartamentoReceptor() {
+        return departamentoReceptor;
+    }
+
+    public void setDepartamentoReceptor(String departamentoReceptor) {
+        this.departamentoReceptor = departamentoReceptor;
+    }
+
+    public double getLatitudReceptor() {
+        return latitudReceptor;
+    }
+
+    public void setLatitudReceptor(double latitudReceptor) {
+        this.latitudReceptor = latitudReceptor;
+    }
+
+    public double getLongitudReceptor() {
+        return longitudReceptor;
+    }
+
+    public void setLongitudReceptor(double longitudReceptor) {
+        this.longitudReceptor = longitudReceptor;
+    }
+
+    public Date getFechaRecibido() {
+        return fechaRecibido;
+    }
+
+    public void setFechaRecibido(Date fechaRecibido) {
+        this.fechaRecibido = fechaRecibido;
+    }
+
+    public Date getFechaEnvio() {
+        return fechaEnvio;
+    }
+
+    public void setFechaEnvio(Date fechaEnvio) {
+        this.fechaEnvio = fechaEnvio;
+    }
+
+    public Date getHoraRecibido() {
+        return horaRecibido;
+    }
+
+    public void setHoraRecibido(Date horaRecibido) {
+        this.horaRecibido = horaRecibido;
+    }
+
+    public Date getHoraEnvio() {
+        return horaEnvio;
+    }
+
+    public void setHoraEnvio(Date horaEnvio) {
+        this.horaEnvio = horaEnvio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    
+    
+    
+}
